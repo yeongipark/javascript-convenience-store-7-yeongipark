9f0040ec6a507f9bd4e58b1e1eaf9f79023ebcef
Yeongi Park <yeongi0111@naver.com>
2024-11-10 15:29:01 +0900

    feat(StoreService): StoreService 구현
    - 여러 모델의 상호작용이 필요한 비즈니스 로직 구현
    - 어플리케이션의 핵심 로직을 담당

---
+++ __tests__/StoreServiceTest.js
@@ -0,0 +1,47 @@
+import { ERROR_MESSAGE } from '../src/constants/errorMessage.js';
+import StoreService from '../src/service/StoreService.js';
+
+describe('StoreService 테스트 실행', () => {
+  let storeService;
+  beforeEach(async () => {
+    storeService = new StoreService();
+    await storeService.init();
+  });
+
+  test.each([
+    ['[바나나바나나-6]', ERROR_MESSAGE.NOT_EXIST_PRODUCT],
+    ['[콜라-1000]', ERROR_MESSAGE.OVER_QUANTITY],
+    ['[바나나바나나]', ERROR_MESSAGE.NOT_MATCH_FORMAT],
+    ['[콜라-10],[바나나바나나-100]', ERROR_MESSAGE.NOT_EXIST_PRODUCT],
+    ['', ERROR_MESSAGE.FALSE_INPUT],
+  ])('%s 사용자의 구매 입력이 잘못된 경우', (input, error) => {
+    expect(() => storeService.handleInputOrder(input)).toThrow(error);
+  });
+
+  test('사용자 입력이 유효한 경우', () => {
+    const inputOrder = '[콜라-10],[사이다-10]';
+    expect(() => storeService.handleInputOrder(inputOrder)).not.toThrow('[ERROR]');
+  });
+
+  test.each([
+    ['[콜라-5],[사이다-5]', { 콜라: 1, 사이다: 1 }],
+    ['[콜라-6],[사이다-5]', { 사이다: 1 }],
+    ['[물-5],[에너지바-3],[콜라-5]', { 콜라: 1 }],
+    ['[물-5],[에너지바-5]', {}],
+    ['[초코바-3]', { 초코바: 1 }],
+  ])('%s 프로모션 진행 중인 물품중 혜택만큼 가져오지 않은 경우', (input, expected) => {
+    storeService.handleInputOrder(input);
+    expect(storeService.getNotEnoughPromotionProduct()).toEqual(expected);
+  });
+
+  test.each([
+    ['[콜라-15]', { 콜라: 6 }],
+    ['[콜라-10]', { 콜라: 1 }],
+    ['[물-5],[콜라-15],[정식도시락-3]', { 콜라: 6 }],
+    ['[콜라-15],[사이다-9]', { 콜라: 6, 사이다: 3 }],
+    ['[초코바-5]', { 초코바: 1 }],
+  ])('%s 프로모션 진행 중인 물품 중 정가로 구매해야 되는 상품명과 갯수', (input, expected) => {
+    storeService.handleInputOrder(input);
+    expect(storeService.getRegularProductsWithPromotion()).toEqual(expected);
+  });
+});
---
+++ src/service/StoreService.js
@@ -0,0 +1,195 @@
+import Products from '../model/Products.js';
+import Promotions from '../model/Promotions.js';
+import Validator from '../Validator.js';
+import Order from '../model/Order.js';
+import { parseInputOrder } from '../util/util.js';
+import { OUTPUT_MESSAGE } from '../constants/outputMessage.js';
+
+export default class StoreService {
+  #promotions;
+  #products;
+  #order;
+
+  constructor() {
+    this.#promotions = new Promotions();
+    this.#products = new Products();
+    this.#order = null;
+  }
+
+  async init() {
+    await this.#products.init();
+    await this.#promotions.init();
+  }
+
+  getProductsInformationMessage() {
+    const productsList = this.#products.getProductsInformation();
+    return productsList.map((product) => {
+      let message = `- ${product.name} ${product.price.toLocaleString()}원 `;
+      if (product.quantity === 0) message += OUTPUT_MESSAGE.NO_QUANTITY;
+      if (product.quantity !== 0) message += `${product.quantity.toLocaleString()}개`;
+      if (product.promotion !== OUTPUT_MESSAGE.NULL) message += ` ${product.promotion}`;
+      return message;
+    });
+  }
+
+  handleInputOrder(inputOrder) {
+    Validator.validateInputOrder(inputOrder);
+    const parsedInputOrder = parseInputOrder(inputOrder);
+    const productsName = this.#products.getProductsName();
+    this.#validateParsedInputOrder(productsName, parsedInputOrder);
+    this.#order = new Order(parsedInputOrder);
+  }
+
+  #validateParsedInputOrder(productsName, parsedInputOrder) {
+    Validator.validateProductExist(productsName, parsedInputOrder);
+    Object.entries(parsedInputOrder).forEach(([productName, orderCount]) => {
+      const productQuantity = this.#products.getProductQuantity(productName);
+      Validator.validateQuantity(productQuantity, orderCount);
+    });
+  }
+
+  getNotEnoughPromotionProduct() {
+    const orderList = this.#order.getOrderList();
+    return orderList.reduce((notEnoughProducts, [productName, orderCount]) => {
+      const promotionInfo = this.#getPromotionInfo(productName);
+      if (!promotionInfo) return notEnoughProducts;
+      const { promotionQuantity, buy, get } = promotionInfo;
+      if (promotionQuantity > orderCount && orderCount % (buy + get) === buy) notEnoughProducts[productName] = get;
+      return notEnoughProducts;
+    }, {});
+  }
+
+  handleFreeQuantityAnswer(answer, productName, get) {
+    Validator.validateAnswer(answer);
+    if (answer.toUpperCase() === 'Y') this.#order.increaseOrderQuantity(productName, get);
+  }
+
+  getRegularProductsWithPromotion() {
+    const orderList = this.#order.getOrderList();
+    return orderList.reduce((regularPriceProducts, [productName, orderCount]) => {
+      const promotionInfo = this.#getPromotionInfo(productName);
+      if (!promotionInfo) return regularPriceProducts;
+      const { promotionQuantity, buy, get } = promotionInfo;
+      const maxPossiblePromotionCount = this.#calculateMaxPossiblePromotionCount(promotionQuantity, buy, get);
+      if (orderCount - maxPossiblePromotionCount > 0)
+        regularPriceProducts[productName] = orderCount - maxPossiblePromotionCount;
+      return regularPriceProducts;
+    }, {});
+  }
+
+  handleRegularProductsAnswer(answer, productName, count) {
+    Validator.validateAnswer(answer);
+    if (answer.toUpperCase() === 'N') this.#order.decreaseOrderQuantity(productName, count);
+  }
+
+  handleMembershipAnswer(answer) {
+    Validator.validateAnswer(answer);
+    if (answer.toUpperCase() === 'Y') this.#order.setDiscount(this.#calculateMembershipDiscount());
+  }
+
+  #calculateMembershipDiscount() {
+    const regularProducts = this.#getRegularQuantityObject();
+    const totalRegularPrice = Object.entries(regularProducts).reduce((totalPrice, [productName, orderCount]) => {
+      return totalPrice + this.#products.getProductPrice(productName) * orderCount;
+    }, 0);
+    return Math.min(totalRegularPrice * 0.3, 8000);
+  }
+
+  #getRegularQuantityObject() {
+    return this.#order.getOrderList().reduce((regularProducts, [productName, orderCount]) => {
+      const maxPromotionCount = this.#calculateMaxPromotionCount(productName, orderCount);
+      regularProducts[productName] = orderCount - maxPromotionCount;
+      return regularProducts;
+    }, {});
+  }
+
+  #calculateMaxPromotionCount(productName, orderCount) {
+    const promotionInfo = this.#getPromotionInfo(productName);
+    if (!promotionInfo) return 0;
+    const { promotionQuantity, buy, get } = promotionInfo;
+    const maxBuyPromotionCount = Math.min(promotionQuantity, orderCount);
+    return this.#calculateMaxPossiblePromotionCount(maxBuyPromotionCount, buy, get);
+  }
+
+  #calculateMaxPossiblePromotionCount(promotionQuantity, buy, get) {
+    return promotionQuantity - (promotionQuantity % (buy + get));
+  }
+
+  handleContinueAnswer(answer) {
+    Validator.validateAnswer(answer);
+    if (answer.toUpperCase() === 'N') return false;
+    return true;
+  }
+
+  #getPromotionInfo(productName) {
+    const promotionName = this.#products.getProductPromotion(productName);
+    if (!this.#promotions.isValidatePromotion(promotionName)) return null;
+    const promotionQuantity = this.#products.getPromotionProductQuantity(productName);
+    const { buy, get } = this.#promotions.getBuyGetCount(promotionName);
+    return { promotionName, promotionQuantity, buy, get };
+  }
+
+  reduceQuantity() {
+    const orderList = this.#order.getOrderList();
+    orderList.forEach(([productName, quantity]) => {
+      const promotionName = this.#products.getProductPromotion(productName);
+      const isPromotion = this.#promotions.isValidatePromotion(promotionName);
+      this.#products.reduceProductQuantity(productName, quantity, isPromotion);
+    });
+  }
+
+  getOrderList() {
+    return this.#order
+      .getOrderList()
+      .filter(([_, quantity]) => quantity > 0)
+      .map(([name, quantity]) => ({
+        name,
+        quantity,
+        price: this.#products.getProductPrice(name) * quantity,
+      }));
+  }
+
+  getGiveawayList() {
+    return Object.entries(this.#getGiveawayObject()).map(([name, quantity]) => ({ name, quantity }));
+  }
+
+  #getGiveawayObject() {
+    const orderList = this.#order.getOrderList();
+    return orderList.reduce((giveawayObject, [productName, orderCount]) => {
+      const maxPromotionCount = this.#calculateMaxPromotionCount(productName, orderCount);
+      if (!maxPromotionCount) return giveawayObject;
+      const { buy, get } = this.#getPromotionInfo(productName);
+      giveawayObject[productName] = maxPromotionCount / (buy + get);
+      return giveawayObject;
+    }, {});
+  }
+
+  getReceiptData() {
+    const totalOrderQuantity = this.#calculateTotalOrderQuantity();
+    const totalPrice = this.#calculateTotalPrice();
+    const promotionDiscount = this.#calculatePromotionDiscount();
+    const membershipDiscount = this.#order.getDiscount();
+    const finalPrice = totalPrice - promotionDiscount - membershipDiscount;
+    return { totalOrderQuantity, totalPrice, promotionDiscount, finalPrice, membershipDiscount };
+  }
+
+  #calculateTotalOrderQuantity() {
+    return this.#order.getOrderList().reduce((totalQuantity, [_, quantity]) => {
+      return totalQuantity + quantity;
+    }, 0);
+  }
+
+  #calculatePromotionDiscount() {
+    return this.getGiveawayList().reduce((totalDiscount, { name, quantity }) => {
+      return totalDiscount + this.#products.getProductPrice(name) * quantity;
+    }, 0);
+  }
+
+  #calculateTotalPrice() {
+    return this.#order.getOrderList().reduce((totalPrice, [productName, quantity]) => {
+      const productPrice = this.#products.getProductPrice(productName);
+      const itemTotal = productPrice * quantity;
+      return totalPrice + itemTotal;
+    }, 0);
+  }
+}
