c93012db69753d8652802cea5ed0f20cab8fa0db
Yeongi Park <yeongi0111@naver.com>
2024-11-11 01:47:24 +0900

    feat(util): 중복된 상품이 입력된 경우 예외 처리
    중복된 상품 입력시 예외 처리

--- src/util/util.js
+++ src/util/util.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from '../constants/errorMessage.js';
+
 const QUANTITY_SEPARATOR = '-';
 const SPLIT_STRING = '],[';
 
@@ -7,6 +9,7 @@ export function parseInputOrder(inputOrder) {
     .split(SPLIT_STRING)
     .reduce((result, item) => {
       const [name, quantity] = item.split(QUANTITY_SEPARATOR);
+      if (result[name]) throw new Error(ERROR_MESSAGE.FALSE_INPUT);
       result[name] = Number(quantity);
       return result;
     }, {});
